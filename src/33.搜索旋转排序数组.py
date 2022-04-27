f2462a3b684fe4ad6bf48641b06a2941686b5846
HuangZhuo <idorable@163.com>
2022-04-28 01:07:24 +0800

    33

--- src/33.搜索旋转排序数组.py
+++ src/33.搜索旋转排序数组.py
@@ -15,8 +15,9 @@ class Solution:
             mid = (l + r) >> 1
             if nums[mid] == target:
                 return mid
-            if nums[0] < nums[mid]:
+            if nums[0] <= nums[mid]:  # 单个元素也认为有序
                 # 左侧有序 7,8,9|10,1,2,3
+                # 解在左侧出现的条件
                 if nums[0] <= target < nums[mid]:
                     r = mid - 1
                 else:
@@ -32,6 +33,7 @@ class Solution:
 
 
 # @lc code=end
+print(Solution().search([3, 1], 1))
 print(Solution().search([4, 5, 6, 7, 0, 1, 2], 0))
 print(Solution().search([4, 5, 6, 7, 0, 1, 2], 3))
 print(Solution().search([4, 5, 6, 7, 8, 1, 2, 3], 8))
